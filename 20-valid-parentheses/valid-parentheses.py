ca0116bbcb770233f5c6ece209de4406f8c73009
Sai Satvik Vuppala <50795728+satvik1406@users.noreply.github.com>
2025-03-12 02:42:43 -0400

    Time: 1 ms (32.71%) | Memory: 17.9 MB (32.41%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -3,15 +3,10 @@ class Solution:
         stack = []
         m = {"(":")", "{":"}","[":"]"}
         for num in range(0, len(s)):
-            if s[num] == "(" or s[num] == "{" or s[num] == "[":
+            if s[num] in "({[":
                 stack.append(s[num])
             else:
-                if len(stack) > 0:
-                    pop_el = stack.pop()
-                    print(pop_el)
-                    if m[pop_el] != s[num]:
-                        return False
-                else:
+                if len(stack) == 0 or m[stack.pop()] != s[num]:
                     return False
 
         if len(stack) > 0: return False         
